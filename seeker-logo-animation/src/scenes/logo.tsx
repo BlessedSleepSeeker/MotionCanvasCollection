93d86e8d88c51e1676d703ef90cfb0ca925cf813
BlessedSleepSeeker <27480916+BlessedSleepSeeker@users.noreply.github.com>
2023-12-13 16:58:38 +0100

    [SEEKER LOGO] tested easing animation without success

--- seeker-logo-animation/src/scenes/logo.tsx
+++ seeker-logo-animation/src/scenes/logo.tsx
@@ -1,5 +1,5 @@
 import {Img, Node, Rect, makeScene2D} from '@motion-canvas/2d';
-import {Vector2, all, createRef, sequence, waitFor} from '@motion-canvas/core';
+import {Vector2, all, createRef, easeInSine, sequence, tween, waitFor} from '@motion-canvas/core';
 import sleepoof from '../img/RealSleepoof.png'
 import insect from '../img/readyforbattle1000.png'
 import dog from '../img/doggo.png'
@@ -61,7 +61,7 @@ export default makeScene2D(function* (view) {
           position={[-250, 200]}
         />
       </Node>
-      <Img ref={valueRef} src={ImageSource} size={[900, 900]} compositeOperation={'source-in'}/>
+      {/*<Img ref={valueRef} src={ImageSource} size={[900, 900]} compositeOperation={'source-in'}/>*/}
     </Node>,
   );
   yield* all(
@@ -72,6 +72,7 @@ export default makeScene2D(function* (view) {
     square5().offset([-1, 1], 0),
     square6().offset([-1, 1], 0),
   )
+  //changeOffset(new Vector2(-1, 1))
   yield* sequence(
     0.1,
     square6().size([100, 100], 0.5),
@@ -82,30 +83,31 @@ export default makeScene2D(function* (view) {
     square1().size([100, 100], 0.5),
   )
   yield* all(
-    square1().size([600, 100], 1),
-    square2().position([250, -200], 1),
-    square3().position([150, -100], 1),
-    square4().position([50, 0], 1),
-    square5().position([-50, 100], 1),
-    square6().size([200, 100], 1),
+    // tween(0.5, value => {square1().size(Vector2.lerp(new Vector2(100, 100), new Vector2(600, 100), easeInSine(value),))}),
+    // tween(0.5, value => {square2().position(Vector2.lerp(new Vector2(-250, -200), new Vector2(250, -200), easeInSine(value),))}),
+    // tween(0.5, value => {square3().position(Vector2.lerp(new Vector2(-250, -100), new Vector2(150, -100), easeInSine(value),))}),
+    // tween(0.5, value => {square4().position(Vector2.lerp(new Vector2(-250, 0), new Vector2(50, 0), easeInSine(value),))}),
+    // tween(0.5, value => {square5().position(Vector2.lerp(new Vector2(-250, 100), new Vector2(-50, 100), easeInSine(value),))}),
+    // tween(0.5, value => {square6().size(Vector2.lerp(new Vector2(100, 100), new Vector2(200, 100), easeInSine(value),))}),
+    square1().size([600, 100], 0.5),
+    square2().position([250, -200], 0.5),
+    square3().position([150, -100], 0.5),
+    square4().position([50, 0], 0.5),
+    square5().position([-50, 100], 0.5),
+    square6().size([200, 100], 0.5),
   )
   
-  yield* waitFor(0.5)
-  square1().moveOffset(new Vector2(1, 1)),
-  square2().moveOffset(new Vector2(1, 1)),
-  square3().moveOffset(new Vector2(1, 1)),
-  square4().moveOffset(new Vector2(1, 1)),
-  square5().moveOffset(new Vector2(1, 1)),
-  square6().moveOffset(new Vector2(1, 1)),
+  yield* waitFor(1)
+  changeOffset(new Vector2(1, 1))
 
   yield* all(
-    square1().size([100, 100], 1),
-    square2().position([350, -200], 1),
-    square3().position([350, -100], 1),
-    square4().position([350, 0], 1),
-    square5().position([350, 100], 1),
-    square6().size([100, 100], 1),
-    square6().position([350, 200], 1),
+    square1().size([100, 100], 0.5),
+    square2().position([350, -200], 0.5),
+    square3().position([350, -100], 0.5),
+    square4().position([350, 0], 0.5),
+    square5().position([350, 100], 0.5),
+    square6().size([100, 100], 0.5),
+    square6().position([350, 200], 0.5),
   )
   // end
   yield* sequence(
@@ -117,4 +119,13 @@ export default makeScene2D(function* (view) {
     square5().size([100, 0], 0.5),
     square6().size([100, 0], 0.5),
   )
+
+  function changeOffset(new_offset: Vector2) {
+    square1().moveOffset(new_offset);
+    square2().moveOffset(new_offset);
+    square3().moveOffset(new_offset);
+    square4().moveOffset(new_offset);
+    square5().moveOffset(new_offset);
+    square6().moveOffset(new_offset);
+  }
 });
